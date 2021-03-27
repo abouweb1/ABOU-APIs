5c45cea76ba21b02968fd6ed47306bb017a3b71d
Mohaned Kohil <mohaned.kohil11@gmail.com>
2021-03-27 22:29:31 +0200

    Procfile

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(basicAuth({ users: { 'admin': 'admin@2020' } }));
+app.use(basicAuth({ users: { 'admin': 'admin@2021' } }));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -35,7 +35,7 @@ app.use('/messages', messages);
 
 
 let port = process.env.PORT;
-let DB = ''
+let DB = 'mongodb+srv://admin-ABOU:ABOU@2021@abou-db.cz1ev.mongodb.net/abou-db'
 if (port == null || port == "") {
     port = 3000;
     DB = 'mongodb://localhost:27017/ABOU'
